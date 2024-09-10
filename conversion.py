c41e27248b754f04a6c21faa9c42b321922cb453
Saliha Saqib <salihasaqib11@gmail.com>
2024-09-10 10:01:29 +0100

    Add files via upload
    Basic tasks

---
+++ conversion.py
@@ -0,0 +1,23 @@
+# declare all the variables 
+#num1 = 99.23
+#num2 = 23
+#num3 = 150
+#string1 = “100”
+#Convert all the variables using type convertion
+#num1 into an integer
+#num2 into a float
+#num3 into a string
+#string1 into an integer
+#Print out all the variables on separate lines using separate print statments
+num1= 99.23
+num2= 23
+num3= 150
+string1= "100"
+num1= int(num1)
+num2= float(num2)
+num3= str(num3)
+string1= int(string1)
+print(f"num1= {num1}")
+print(f"num2= {num2}")
+print(f"num3= {num3}")
+print(f"string1= {string1}")
---
+++ hello_world.py
@@ -0,0 +1,10 @@
+#get user name as input
+#print the name as output
+#get user age as input
+#print user age as output
+#Print string Hello World!
+name=input("Enter your name ")
+print("User name is ",name)
+age=int(input("Enter your age "))
+print(f"User age is {age}")
+print("Hello World!")
